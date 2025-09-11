fb86b3e39bebb3fc0a685f9e4e94ba614c1a13d5
Col-Umbo <icefirefish418@gmail.com>
2025-09-11 11:36:23 -0400

    Removing default parameters

--- col-umbo.github.io/src/App.tsx
+++ col-umbo.github.io/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './teppei.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Work in progress. Ignore.
+        </h1>
+        <img src={image} className="wip-gif" alt="It's really broken if you're seeing this" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.SAMPLE TEXT
         </p>
