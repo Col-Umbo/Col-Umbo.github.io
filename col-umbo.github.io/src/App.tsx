a08e0c6934aca4b8b7f97fc7dab11ac139cf447c
Col-Umbo <icefirefish418@gmail.com>
2025-09-16 20:51:22 -0400

    updated app.tsx

--- col-umbo.github.io/src/App.tsx
+++ col-umbo.github.io/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import image from './teppei.gif';
 import './App.css';
 
 function App() {
@@ -7,20 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>
-          Work in progress. Ignore.
+          Gunpla
         </h1>
-        <img src={image} className="wip-gif" alt="It's really broken if you're seeing this" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Sample text regarding model kits
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
