14eca8f496cc78e7d39fcae300a123aa4043abed
Col-Umbo <icefirefish418@gmail.com>
2025-09-16 22:26:35 -0400

    Site overhaul

--- col-umbo.github.io/src/App.tsx
+++ col-umbo.github.io/src/App.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import './App.css';
-
+import logo from './assets/Gunpla_Logo.png';
+import ImageGallery from "react-image-gallery";
+import img1 from "./assets/IMG_6356.jpeg";
+import img2 from "./assets/IMG_6359.jpeg";
+import img3 from "./assets/IMG_6360.jpeg";
+import img4 from './assets/IMG_6361.jpeg';
+import "react-image-gallery/styles/css/image-gallery.css";
 function App() {
+  const images = [
+  {
+    original: img1
+  },
+  {
+    original: img2
+  },
+  {
+    original: img3
+  },
+  {
+    original: img4
+  }
+];
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          Gunpla
-        </h1>
-        <p>
-          Sample text regarding model kits
-        </p>
-        
+          <img src={logo} alt="Gunpla logo" className='logo'/>
+          <p>
+            Subheader regarding model kits
+          </p>
       </header>
+      <div className='App-body'>
+        <ImageGallery items={images} showThumbnails={false} showPlayButton={false} autoPlay={true}/>
+      </div>
     </div>
   );
 }
